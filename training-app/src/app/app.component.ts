9b5e0f28dbe61ede6e491373548c9d249e0ce1b4
JbRezidu <duriez.jeanbaptiste@gmail.com>
2017-07-31 14:00:28 +0200

    Premier formulaire

--- training-app/src/app/app.component.ts
+++ training-app/src/app/app.component.ts
@@ -5,6 +5,18 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class User {
+  user: Object
+
+  constructor() {
+    this.user = {
+      lastName: "caby",
+      firstName: "",
+      birthDate: new Date(),
+    }
+  }
+
+  createAccount() {
+    console.log(this.user);
+  }
 }
--- training-app/src/app/app.module.ts
+++ training-app/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
 
-import { AppComponent } from './app.component';
+import { User } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    User
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [User]
 })
 export class AppModule { }
